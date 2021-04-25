2f0ad1bc6f301ee3becd1a6c8b89365067e59f36
surajsharma <surajsharma@pm.me>
2021-04-25 18:01:30 +0530

    updated .gitignore

--- 2.2.1.scm
+++ 2.2.1.scm
@@ -2,7 +2,7 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname 2.2.1) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 (define (fahrenheit celcius)
-  (* (- 32 celcius)(/ 5 9)))
+  (* (- celcius 32)(/ 5 9)))
 
 
 (fahrenheit 100)
